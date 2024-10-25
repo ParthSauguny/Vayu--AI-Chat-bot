238550ef40afda187592a70a5b292fa081f337d2
ParthSauguny <parthsauguny@gmail.com>
2024-10-25 18:16:31 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [error , setError] = useState("");
   
   return (
-    <>
-      
-    </>
+    <div className='flex flex-col justify-center mx-auto bg-gray-400 border-4 border-zinc-950 py-5 mt-20 px-6'>
+      <div className=''>
+        <p1> What do you want to know lil' nigga
+          <button>Surprise Me!</button>
+        </p1>
+
+        <div>
+          <input
+            type="text"
+            placeholder='how can i help you today?'
+            onChange={""}
+          />
+          {!error && <button> Ask </button>}
+          {error && <button> Clear </button>}
+
+        </div>
+        {error && <p>{error}</p>}
+      </div>
+
+      <div>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
